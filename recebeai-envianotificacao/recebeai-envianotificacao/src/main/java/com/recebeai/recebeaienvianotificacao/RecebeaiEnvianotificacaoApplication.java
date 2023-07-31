84aaf03e4efd3a8a86cb689898c9704b42f093f9
matheus <matheusvaladao5@gmail.com>
2023-07-31 10:56:51 -0300

    Adição de Serviço de Envio de E-mail

---
+++ recebeai-envianotificacao/recebeai-envianotificacao/src/main/java/com/recebeai/recebeaienvianotificacao/RecebeaiEnvianotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.recebeai.recebeaienvianotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecebeaiEnvianotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecebeaiEnvianotificacaoApplication.class, args);
+	}
+
+}
---
+++ recebeai-envianotificacao/recebeai-envianotificacao/src/test/java/com/recebeai/recebeaienvianotificacao/RecebeaiEnvianotificacaoApplicationTests.java
@@ -0,0 +1,8 @@
+package com.recebeai.recebeaienvianotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecebeaiEnvianotificacaoApplicationTests {
+}
