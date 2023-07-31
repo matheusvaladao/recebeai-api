b4dfd5f13148cea3399a856fd7b04651a4251e0e
matheus <matheusvaladao5@gmail.com>
2023-07-31 11:18:30 -0300

    Configuração de Consumo de Mensagens

--- recebeai-envianotificacao/recebeai-envianotificacao/src/main/java/com/recebeai/recebeaienvianotificacao/RecebeaiEnvianotificacaoApplication.java
+++ recebeai-envianotificacao/recebeai-envianotificacao/src/main/java/com/recebeai/recebeaienvianotificacao/RecebeaiEnvianotificacaoApplication.java
@@ -1,13 +1,18 @@
 package com.recebeai.recebeaienvianotificacao;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+@EnableRabbit
 @SpringBootApplication
 public class RecebeaiEnvianotificacaoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecebeaiEnvianotificacaoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(RecebeaiEnvianotificacaoApplication.class, args);
+    }
 
 }
